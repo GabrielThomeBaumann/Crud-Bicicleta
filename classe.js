2baf509028daa85e902b7bb4b2adc9feb0b1b16f
GabrielThomeBaumann <138903389+GabrielThomeBaumann@users.noreply.github.com>
2024-11-18 11:32:45 -0300

    Add files via upload

---
+++ classe.js
@@ -0,0 +1,10 @@
+class bicicleta {
+    constructor(id, nome, fabricante, data, preco, peso) {
+        this.id = id;
+        this.nome = nome;
+        this.fabricante = fabricante;
+        this.data = data;
+        this.preco = preco;
+        this.peso = peso;
+    }
+}
---
+++ controler.js
@@ -0,0 +1,12 @@
+var lista = "";
+
+function inserir(){
+    var id = parseInt(document.getElementById("id").value);
+    var nome = document.getElementById("nome").value;
+    var fabricante = document.getElementById("fabricante").value;
+    var data = document.getElementById("data").value;
+    var preco = parseFloat.getElementById("preco").value;
+    var peso = parseFloat.getElementById("peso").value;
+
+
+}
